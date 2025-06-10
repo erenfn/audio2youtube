466fe535500419341617864d980ded0b9e072fad
erenfn <81182796+erenfn@users.noreply.github.com>
2025-06-10 06:23:54 +0300

    Update and rename next.config.js to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
@@ -44,4 +45,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig 
+export default nextConfig 
